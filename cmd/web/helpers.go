5886bc538bef9a9ef75b9063123593f2624104ab
cgoolsby <cgoolsby@uark.edu>
2019-09-15 08:45:12 -0700

    added default data to template data in page rendering for more dynamic pages

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,9 +5,9 @@ import (
   "fmt"
   "bytes"
   "runtime/debug"
+  "time"
 )
 
-//function which renders the template files from the new templateCache into a buffer, then writes to the http.Responsewriter
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
   ts, ok := app.templateCache[name]
   if !ok {
@@ -17,7 +17,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
   buffer := new(bytes.Buffer) //buffer the page before write
 
-  err := ts.Execute(buffer, td)
+  err := ts.Execute(buffer, app.addDefaultData(td, r))
   if err != nil {
     app.serverError(w, err)
     return
@@ -30,7 +30,21 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
   trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
   app.errorLog.Output(2, trace)
   http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-  }
+}
 func (app *application) clientError(w http.ResponseWriter, status int) {
   http.Error(w, http.StatusText(status), status)
+}
+func (app *application) isAuthenticated(r *http.Request) bool {
+  return app.session.Exists(r, "authenticatedUserID")
+}
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+  if td == nil {
+    td = &templateData{}
   }
+
+  td.CurrentYear = time.Now().Year()
+
+  td.IsAuthenticated = app.isAuthenticated(r)
+  return td
+}
