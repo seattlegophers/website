b851624807089bb539ed4f0972abc68740e84f85
cgoolsby <cgoolsby@uark.edu>
2019-09-15 01:28:26 -0700

    added authenticate function

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+  "context"
+  "net/http"
+  "seattleGophers.com/website/pkg/models"
+)
+
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    exists := app.session.Exists(r, "authenticatedUserID")
+    if !exists {
+      next.ServeHTTP(w, r)
+      return
+    }
+
+    user, err := app.users.Get(app.session.GetInt(r, "authenticatedUserID"))
+    if err == models.ErrNoRecord || !user.Active {
+      app.session.Remove(r, "authenticatedUserID")
+      next.ServeHTTP(w, r)
+      return
+    } else if err != nil {
+      app.serverError(w, err)
+      return
+    }
+
+    ctx := context.WithValue(r.Context(), contextKeyIsAuthenticated, true)
+    next.ServeHTTP(w, r.WithContext(ctx))
+  })
+}
+
